b0496dc4d5fe8d5b5a60e8f51c564a78373940c5
DmitriChe <juneority@mail.ru>
2020-05-29 20:54:20 +0300

    final hw26 - добавлена пиганация; создан фильтры capitalize для заголовка на странице results.html и фильтр alfajumper ("прагающие буквы") для заповедей блаженств - фильтры реализованны в виде отдельного приложения textstylesapp; добавлен менеджер моделей для активации записей Ico; в модели Ico реализоно вычисление периода действия в днях и выведено на страницы; реализовано создание профиля пользователя по событию post_save; реализован контекстный процессор, генерирующий случайным образом одну из заповедей блаженств нагорной проповеди Христа с выводом в подвал базового шаблона base.html

--- revolucion/usersapp/models.py
+++ revolucion/usersapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save  # сигнал на сразу после сохранения
+from django.dispatch import receiver  # декоратор @receiver для обработки сигнала
 
 
 # Create your models here.
@@ -7,3 +9,15 @@ from django.contrib.auth.models import AbstractUser
 class NewUser(AbstractUser):
     email = models.EmailField(unique=True)  # поле для уникального емейла
     is_author = models.BooleanField(default=False)  # может быть автором, т.е. создавать записи
+
+
+class Profile(models.Model):
+    info = models.TextField(blank=True)  # необязательное поле для доп информации о пользователе
+    user = models.OneToOneField(NewUser, on_delete=models.CASCADE)  # профиль связан с пользователем NewUser как 1 к 1
+
+
+@receiver(post_save, sender=NewUser)
+def create_profile(sender, instance, **kwargs):
+    print('Создан профиль пользователя - по сигналу после его сохранения!')
+    if not Profile.objects.filter(user=instance).exists():
+        Profile.objects.create(user=instance)
