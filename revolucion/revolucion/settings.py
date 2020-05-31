3563e9b1d3e35f005a04c30827a82ec020e46f8e
DmitriChe <juneority@mail.ru>
2020-05-31 22:07:39 +0300

    final hw26 - изменил формат поля имени в модели с 16 на 32

--- revolucion/revolucion/settings.py
+++ revolucion/revolucion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_imlw1xhu&ovqq$2^+c4#wud7#1fv711y+rs5ccn(ro_q)&mm-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,10 +79,19 @@ WSGI_APPLICATION = 'revolucion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# настраиваем базу данных
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'sitedb',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'django',
+        'PASSWORD': 'nu123456',
+        'HOST': 'localhost'
     }
 }
 
