516040b7bc57548e6f7bffd7641573417b7e3fca
DmitriChe <juneority@mail.ru>
2020-06-01 23:02:42 +0300

    initial hw 30 - прикрутил сервис django-cleanup, притупаем к созданию api

--- revolucion/revolucion/settings.py
+++ revolucion/revolucion/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'icoapp',
     'usersapp',
-    'textstyleapp'
+    'textstyleapp',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
