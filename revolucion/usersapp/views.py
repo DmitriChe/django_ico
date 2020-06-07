a078f0d6ad6071c5cf4b25c06fe00427183458e7
DmitriChe <juneority@mail.ru>
2020-06-07 12:31:47 +0300

    initial hw 31 - назначены права админу просматривать и добавлять токены - добавлена авторизация по токену - добавлена генерация токена на странице пофиля пользователя

--- revolucion/usersapp/views.py
+++ revolucion/usersapp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView, DetailView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .forms import RegistrationForm
 from .models import NewUser
 from rest_framework.authtoken.models import Token
@@ -24,3 +25,29 @@ class UserCreateView(CreateView):
 class UserDetailView(DetailView):
     template_name = 'usersapp/profile.html'
     model = NewUser
+
+
+def update_token(request):
+    user = request.user
+    if user.auth_token:
+        # обновить токен, если был
+        user.auth_token.delete()
+        Token.objects.create(user=user)
+    else:
+        # создать токен, если не было
+        Token.objects.create(user=user)
+
+    return HttpResponseRedirect(reverse('users:profile', kwargs={'pk': user.pk}))
+
+
+def update_token_ajax(request):
+    user = request.user
+    if user.auth_token:
+        # обновить токен, если был
+        user.auth_token.delete()
+        token = Token.objects.create(user=user)
+    else:
+        # создать токен, если не было
+        token = Token.objects.create(user=user)
+
+    return JsonResponse({'key': token.key})  # отдает токен в js как ответ сервера data для вставки в станицу
